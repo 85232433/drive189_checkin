6744c6fddb1b026e4d9519c4953e6a2cfb1fed02
85232433 <85232433@qq.com>
2025-02-11 16:01:40 +0800

    Update env.js

--- env.js
+++ env.js
@@ -6,6 +6,6 @@ module.exports = {
     FAMILY_ID: process.env.TYY_FAMILY_ID || "",
     TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID || "",
     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || "",
-    WX_PUSHER_APP_TOKEN: process.env.WX_PUSHER_APP_TOKEN || "AT_OzwCik0QP4p4AQlKdO4jbgjWApTiWsJr",
+    WX_PUSHER_APP_TOKEN: process.env.WX_PUSHER_APP_TOKEN || "AT_amsxhtPmHb6NDoybOatZaVp5nQyadKnl",
     WX_PUSHER_UID: process.env.WX_PUSHER_UID || "",
 };
